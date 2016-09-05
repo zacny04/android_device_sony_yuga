ab465e3d99f6094740ff8874a6046e32f5f6b4cb
zacny04 <bartek10041996@wp.pl>
2016-09-05 18:58:49 +0200

    add Aosp build support

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,2 +1,2 @@
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/full_yuga.mk
+     $(LOCAL_DIR)/aosp_yuga.mk
--- aosp_yuga.mk
+++ aosp_yuga.mk
@@ -1,12 +1,6 @@
 $(call inherit-product, device/sony/yuga/full_yuga.mk)
 
-# Enhanced NFC
-$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
-
-# Inherit CM common Phone stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
-
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=C6603 BUILD_FINGERPRINT=Sony/C6603/C6603:5.1.1/10.7.A.0.228/58103698:user/release-keys PRIVATE_BUILD_DESC="C6603-user 5.1.1 10.7.A.0.228 58103698 release-keys"
 
-PRODUCT_NAME := cm_yuga
+PRODUCT_NAME := aosp_yuga
 PRODUCT_DEVICE := yuga
